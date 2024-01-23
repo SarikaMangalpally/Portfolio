abbda808f056709bd84ed9fe51d0a66ca5e7d7aa
sarika <sarikamangalpally@gmail.com>
2024-01-22 19:56:55 -0600

    add existing project files to git

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Footer from './components/footer'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello Sarika! Welcome to your Portfolio</h1>
+      <Footer />
+    </>
+  )
+}
+
+export default App
