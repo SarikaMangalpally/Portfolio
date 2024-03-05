1d7bb01da1d3540fd30de2b9c2e75f7584ff6d01
sarika <sarikamangalpally@gmail.com>
2024-03-05 11:08:24 -0600

    created route to hire me form and updated resume link

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import Footer from './components/footer'
 import Header from './components/header'
 import Home from './pages/home'
+import HireMe from './pages/hireMe'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <main className='relative sm:h-full'>
-      <Header className=""/>
-      <div className="relative sm:ml-16">
-        <Home className=""/>
-        <Footer className=""/>
-      </div>
-    </main>
+    <Router>
+      <main className='relative sm:h-full'>
+        <Header className=""/>
+        <div className="relative sm:ml-16">
+          <Routes>
+            <Route path='/' element={<Home/>}></Route>
+            <Route path='/hire-me' element={<HireMe title='hire-me'/>}></Route>
+              {/* <Home className=""/> */}
+            
+          </Routes>
+          <Footer className=""/>
+        </div>
+      </main>
+    </Router>
   )
 }
 
--- src/pages/hireMe.jsx
+++ src/pages/hireMe.jsx
@@ -1,6 +1,16 @@
 import React from 'react'
-export default function HireMe() {
+export default function HireMe(props) {
+    const title = props.title
     return (
-        <h2>Hire Me page to be built</h2>
+        <div className='mx-auto'>
+            <h2 className='Capitalize font-oswald-light text-32 xs:text-36 lg:text-40 xl:text-54'>
+                {title}
+            </h2>
+            <div>
+                <form>
+
+                </form>
+            </div>
+        </div>
     )
 }
